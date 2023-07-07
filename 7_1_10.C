d8a4b9a7eea4720205399a27a5d491dc4b015594
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-07 11:58:59 +0530

    Update 7_1_10.C

--- 7_1_10.C
+++ 7_1_10.C
@@ -1,1 +1,27 @@
-#
+Q.10 Write a Program to print the below pattern using nested for loop.
+          
+*/          
+  output :
+            1
+          1 2 3
+        1 2 3 3 4 
+      1 2 3 4 5 6 7
+    1 2 3 4 5 6 7 8 9
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int i,j,e = 5;
+clrscr();
+
+for (int i = 0; i < rows; i++) {
+for (int j = 0; j < 2 * (rows - i) - 1; j++) {
+printf(" ");}
+for (int k = 0; k < 2 * i + 1; k++) {
+printf("%d ", k + 1);}
+printf("\n");}
+    getch();
+}
