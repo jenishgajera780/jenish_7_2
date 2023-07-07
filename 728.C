e59b5991d5fd94984cfd9b85a56b222784f30809
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-07 11:45:55 +0530

    Update 728.C

--- 728.C
+++ 728.C
@@ -1,3 +1,24 @@
+
+Q.8 Write a Program to print the below pattern using nested for loop.
+ 
+/*
+
+output ;
+
+1 2 3 4 5 5 4 3 2 1
+1 2 3 4     4 3 2 1
+1 2 3         3 2 1
+1 2             2 1
+1                 1
+1                 1
+1 2             2 1
+1 2 3         3 2 1
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1
+
+
+*/
+
 #include<stdio.h>
 #include<conio.h>
 
