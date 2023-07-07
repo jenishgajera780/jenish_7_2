d150a36cb3e29b971349fb68542ab6fb60badbd9
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-07 11:03:05 +0530

    Update 7_1_11.C

--- 7_1_11.C
+++ 7_1_11.C
@@ -1,1 +1,30 @@
-#
+// Q.11 Write a Program to print the below pattern using nested for loop.
+
+/*
+
+         *         
+       * * *                 
+     * * * * *              
+   * * * * * * *          
+ * * * * * * * * *   
+
+*/
+
+
+#include <stdio.h>
+#include <conio.h> 
+
+void main(){
+
+int i,j,e = 5;
+
+for(int i = 0;i<e;i++){
+for(int j = 0;j<2*(e-i)-1;j++){
+printf(" ");}
+        
+for (int k = 0; k < 2 * i + 1; k++) {
+    printf(" *");}
+    printf("\n");}
+    
+    getch();
+}
